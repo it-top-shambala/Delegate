9b8390b4dc759fd333118dc85c2a8ef4ed483fa7
Starinin Andrey <starininandrey@gmail.com>
2022-01-12 20:17:12 +0300

    Add Lib

---
+++ Delegate.Lib/Calculate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Delegate.Lib
+{
+    public static class Calculate
+    {
+        public static Message Log;
+        
+        public static int Add(int x, int y)
+        {
+            Log($"x = {x}, y = {y}");
+            var result = x + y;
+            Log($"{x} + {y} = {result}");
+            return result;
+        }
+        
+        public static int Sub(int x, int y)
+        {
+            Log($"x = {x}, y = {y}");
+            var result = x - y;
+            Log($"{x} - {y} = {result}");
+            return result;
+        }
+        
+        public static int Mul(int x, int y)
+        {
+            Log($"x = {x}, y = {y}");
+            var result = x * y;
+            Log($"{x} * {y} = {result}");
+            return result;
+        }
+        
+        public static int Div(int x, int y)
+        {
+            Log($"x = {x}, y = {y}");
+            try
+            {
+                var result = x / y;
+                Log($"{x} / {y} = {result}");
+                return result;
+            }
+            catch (DivideByZeroException)
+            {
+                Log("ERROR Деление на ноль");
+                throw new Exception("Деление на ноль");
+            }
+        }
+    }
+}
---
+++ Delegate.Lib/Message.cs
@@ -0,0 +1,4 @@
+﻿namespace Delegate.Lib
+{
+    public delegate void Message(string message);
+}
