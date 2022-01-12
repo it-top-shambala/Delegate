550d8168d913a0d227ba3e38ccefea699b0d075b
Starinin Andrey <starininandrey@gmail.com>
2022-01-12 21:00:34 +0300

    Update app

--- Delegate.App/Program.cs
+++ Delegate.App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Delegate.Lib;
 
 namespace Delegate.App
 {
@@ -6,7 +7,59 @@ namespace Delegate.App
     {
         private static void Main()
         {
+            CLI.Show("*** Калькулятор v0.1 ***");
+            Console.WriteLine();
+
+            var exit = false;
+            var select = string.Empty;
+            Calculate.Log = Log.LogToFile.Log;
+            var x = 0;
+            var y = 0;
+            var result = 0;
             
+            do
+            {
+                CLI.Show("Режимы работы:");
+                CLI.Show("1. Сложение");
+                CLI.Show("2. Вычитание");
+                CLI.Show("3. Умножение");
+                CLI.Show("4. Деление");
+                CLI.Show("0. Выход");
+                select = CLI.Input("Выберите режим: ");
+
+                switch (select)
+                {
+                    case "1":
+                        x = Int32.Parse(CLI.Input("Введите первое число"));
+                        y = Int32.Parse(CLI.Input("Введите второе число"));
+                        result = Calculate.Add(x, y);
+                        break;
+                    case "2":
+                        x = Int32.Parse(CLI.Input("Введите первое число"));
+                        y = Int32.Parse(CLI.Input("Введите второе число"));
+                        result = Calculate.Sub(x, y);
+                        break;
+                    case "3":
+                        x = Int32.Parse(CLI.Input("Введите первое число"));
+                        y = Int32.Parse(CLI.Input("Введите второе число"));
+                        result = Calculate.Mul(x, y);
+                        break;
+                    case "4":
+                        x = Int32.Parse(CLI.Input("Введите первое число"));
+                        y = Int32.Parse(CLI.Input("Введите второе число"));
+                        result = Calculate.Div(x, y);
+                        break;
+                    case "0":
+                        exit = true;
+                        break;
+                    default:
+                        CLI.Show("Ввели не тот режим. Повторите ввод.");
+                        break;
+                }
+
+            } while (!exit);
+            
+            CLI.Show("До свидания ...");
         }
     }
 }
