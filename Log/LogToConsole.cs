f3d03be1b258a5aeccb42be553075259457ca220
Starinin Andrey <starininandrey@gmail.com>
2022-01-12 20:35:56 +0300

    Add Log proj

---
+++ Log/LogToConsole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Log
+{
+    public static class LogToConsole
+    {
+        public static void Log(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"[INFO] {message}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Log/LogToFile.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace Log
+{
+    public static class LogToFile
+    {
+        public static void Log(string message)
+        {
+            using var file = new StreamWriter("delegate.log", append:true);
+            file.WriteLine($"[INFO] {message}");
+        }
+    }
+}
