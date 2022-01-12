ff5d80437985b6cd5440876001d4a0fed196e593
Starinin Andrey <starininandrey@gmail.com>
2022-01-12 21:39:14 +0300

    Update

--- Delegate.Lib/Calculate.cs
+++ Delegate.Lib/Calculate.cs
@@ -8,40 +8,44 @@ namespace Delegate.Lib
         
         public static int Add(int x, int y)
         {
-            Log($"x = {x}, y = {y}");
+            if (Log is not null)
+            {
+                Log($"x = {x}, y = {y}");
+            }
+            // Log?.Invoke($"x = {x}, y = {y}");
             var result = x + y;
-            Log($"{x} + {y} = {result}");
+            Log?.Invoke($"{x} + {y} = {result}");
             return result;
         }
         
         public static int Sub(int x, int y)
         {
-            Log($"x = {x}, y = {y}");
+            Log?.Invoke($"x = {x}, y = {y}");
             var result = x - y;
-            Log($"{x} - {y} = {result}");
+            Log?.Invoke($"{x} - {y} = {result}");
             return result;
         }
         
         public static int Mul(int x, int y)
         {
-            Log($"x = {x}, y = {y}");
+            Log?.Invoke($"x = {x}, y = {y}");
             var result = x * y;
-            Log($"{x} * {y} = {result}");
+            Log?.Invoke($"{x} * {y} = {result}");
             return result;
         }
         
         public static int Div(int x, int y)
         {
-            Log($"x = {x}, y = {y}");
+            Log?.Invoke($"x = {x}, y = {y}");
             try
             {
                 var result = x / y;
-                Log($"{x} / {y} = {result}");
+                Log?.Invoke($"{x} / {y} = {result}");
                 return result;
             }
             catch (DivideByZeroException)
             {
-                Log("ERROR Деление на ноль");
+                Log?.Invoke("ERROR Деление на ноль");
                 throw new Exception("Деление на ноль");
             }
         }
